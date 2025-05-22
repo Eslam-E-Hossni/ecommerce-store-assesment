b37cdc2c87cec6f9ebee4a7e8d49c750b496c326
Eslam-E-Hossni <eslam.e.hossni@gmail.com>
2025-05-22 23:20:03 +0400

    refactor: remove src directory
    - Removed the `src` directory from the project.
    - Updated file paths accordingly.
    - Added new color palette to tailwind config.
    - Removed unused `types` directory.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins, Roboto } from "next/font/google";
-import "@/styles/globals.css";
+import "../styles/globals.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,47 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          50: "#FFFAE0",
+          100: "#FFF7CC",
+          200: "#FFEFAC",
+          300: "#FFE483",
+          400: "#FFDA64",
+          500: "#FFC831",
+          600: "#DBA523",
+          700: "#B78418",
+          800: "#93650F",
+          900: "#7A4F09",
+        },
+        gray: {
+          25: "#F9FAFB",
+          50: "#F6F7F9",
+          100: "#F3F5F7",
+          200: "#EFF2F5",
+          300: "#DFE3E8",
+          400: "#C4CDD5",
+          500: "#919EAB",
+          600: "#637381",
+          700: "#454F5B",
+          800: "#212B36",
+          900: "#161C24",
+        },
+        white: "#FFFFFF",
+        white_01: "#C1C0E5",
+        white_02: "#919EAB",
+        black: "#000000",
+        black_01: "#15141B",
+        black_02: "#1F1E24",
+        black_03: "#191922",
+        black_04: "#201E2C",
+        black_05: "#191922",
       },
       fontFamily: {
         "heading-font": "var(--font-poppins)",
